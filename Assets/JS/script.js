d90e323ac7bb67ae54bc4c74f2a1ecccd87e2ae8
Matt Reynolds <mattreynolds53@gmail.com>
2021-11-15 15:59:41 -0600

    Created my questions list with answer choices and correct answers

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,1 +1,26 @@
 let quizTimer = document.querySelector("#timer");
+
+let questions = [
+    {
+        question: "In whih book is Sirius Black introduced?";
+        answers: ["The Chamber of Secrets", "The Order of the Phoenix", "The Goblet of Fire", "The Prisoner of Azkaban"];
+        correctAnswer: "The Prisoner of Azkaban";
+
+        question: "According to the book, who gives Harry the gillyweed before he enters into the second task of the Triwizard Tournament in the Goblet of Fire?";
+        answers: ["Albus Dumbledore", "Dobby the House Elf", "Cedric Diggory", "Neville Longbottom"];
+        correctAnswer: "Dobby the House Elf";
+
+        question: "What is the model of the first broom Harry receives ahead of his first quidditch match?";
+        answers: ["Firebolt", "Nimbus 2000", "Nimbus 2001", "Nimbus 3000"];
+        correctAnswer: "Nimbus 2000";
+
+        question: "What was Sirius Black's nickname?";
+        answers: ["Moony", "Wormtail", "Padfoot", "Prongs"];
+        correctAnswer: "Nimbus 2000";
+
+        question: "What were the professions of Hermoine's parents?";
+        answers: ["Dentists", "Doctors", "Professors", "Entrepreneurs"];
+        correctAnswer: "Nimbus 2000";
+    
+    }
+]
