4aa5b85a4fca5c0839153656905ef7288154e83e
Matt Reynolds <mattreynolds53@gmail.com>
2021-11-16 18:08:33 -0600

    got the questions and answer to cycle through, but only the first button is allowing the next set of questions and answer to pop up. Will add more if statements for other answer buttons

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,15 +3,60 @@ let mainEl = document.querySelector("timerText");
 let secondsLeft = 60;
 let startButton = document.querySelector("#startBtn");
 let timerInterval;
+let totalCorrect = 0;
+let totalIncorrect = 0;
+// Question bank for quiz
+let questions = [
+    {
+        question: "In which book is Sirius Black introduced?",
+        answers: ["The Chamber of Secrets", "The Order of the Phoenix", "The Goblet of Fire", "The Prisoner of Azkaban"],
+        correctAnswer: 3,
+    },
+    {
+        question: "According to the book, who gives Harry the gillyweed before he enters into the second task of the Triwizard Tournament in the Goblet of Fire?",
+        answers: ["Albus Dumbledore", "Dobby the House Elf", "Cedric Diggory", "Neville Longbottom"],
+        correctAnswer: 1,
+    },
+    {
+        question: "What is the model of the first broom Harry receives ahead of his first quidditch match?",
+        answers: ["Firebolt", "Nimbus 2000", "Nimbus 2001", "Nimbus 3000"],
+        correctAnswer: 1,
+    },
+    {
+        question: "What was Sirius Black's nickname?",
+        answers: ["Moony", "Wormtail", "Padfoot", "Prongs"],
+        correctAnswer: 2,
+    },
+    {
+        question: "What were the professions of Hermoine's parents?",
+        answers: ["Dentists", "Doctors", "Professors", "Entrepreneurs"],
+        correctAnswer: 0,
+    
+    }
+]
+
+// Displaying Answer Choices
+
+let currentQuestion = 0;
+let question = questions[currentQuestion];
+
+document.querySelector("#question").textContent = questions[currentQuestion].question;
+document.querySelector("#btn1").textContent = questions[currentQuestion].answers[0];
+document.querySelector("#btn2").textContent = questions[currentQuestion].answers[1];
+document.querySelector("#btn3").textContent = questions[currentQuestion].answers[2];
+document.querySelector("#btn4").textContent = questions[currentQuestion].answers[3];
+
+// Starting the Quiz
 
 startButton.addEventListener("click", function(){
     clearInterval(timerInterval);
     secondsLeft= 60;
 
-    startTimer();
+    startQuiz();
+    
 });
 
-function startTimer(){
+function startQuiz(){
     
     timerInterval = setInterval(function() {
         secondsLeft--;
@@ -26,108 +71,101 @@ function startTimer(){
     
     }, 1000)
 }
+
+
+
+// Event delegation
+
+let answerChoices = document.querySelector("#answerChoices");
+
+// Listen for any clicks within the img-container div
+answerChoices.addEventListener("click", function(event) { 
+    let element = event.target;
+  console.log(element);
+    
+    if (element.matches(".button")) {
+
+      let chosenAnswer = parseInt(element.getAttribute("data-value"));
+      console.log(question);
+      console.log(chosenAnswer)
+  
+    }
+
+  });
+
+// Taking the Quiz
+let questionDisplay = document.querySelector("#question")
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let btn3 = document.querySelector("#btn3");
+let btn4 = document.querySelector("#btn4");
+
+function nextQuestion() {
+    console.log(currentQuestion);
+
+    if (currentQuestion < 4) {
+        questionDisplay.textContent = questions[currentQuestion].question;
+        btn1.textContent = questions[currentQuestion].answers[0]; 
+        btn2.textContent = questions[currentQuestion].answers[1];
+        btn3.textContent = questions[currentQuestion].answers[2];
+        btn4.textContent = questions[currentQuestion].answers[3];
+    } else {
+        console.log("No further questions");
+        // am i missing anything here?
+
+    }
+}
+
+btn1.addEventListener("click", function() {
+    if (btn1.textContent === questions[currentQuestion].correctAnswer) {
+        console.log("Correct answer!");
+        totalCorrect++;
+        currentQuestion++;
+        nextQuestion();
+    } else {
+        console.log("Wrong answer");
+        totalIncorrect++;
+        currentQuestion++;
+        nextQuestion();
+    }
+})
+
+
+
+
+
+
+
   
 
-let questions = [
-    {
-        question: "In which book is Sirius Black introduced?",
-        answers: ["The Chamber of Secrets", "The Order of the Phoenix", "The Goblet of Fire", "The Prisoner of Azkaban"],
-        correctAnswer: "The Prisoner of Azkaban",
-    },
-    {
-        question: "According to the book, who gives Harry the gillyweed before he enters into the second task of the Triwizard Tournament in the Goblet of Fire?",
-        answers: ["Albus Dumbledore", "Dobby the House Elf", "Cedric Diggory", "Neville Longbottom"],
-        correctAnswer: "Dobby the House Elf",
-    },
-    {
-        question: "What is the model of the first broom Harry receives ahead of his first quidditch match?",
-        answers: ["Firebolt", "Nimbus 2000", "Nimbus 2001", "Nimbus 3000"],
-        correctAnswer: "Nimbus 2000",
-    },
-    {
-        question: "What was Sirius Black's nickname?",
-        answers: ["Moony", "Wormtail", "Padfoot", "Prongs"],
-        correctAnswer: "Nimbus 2000",
-    },
-    {
-        question: "What were the professions of Hermoine's parents?",
-        answers: ["Dentists", "Doctors", "Professors", "Entrepreneurs"],
-        correctAnswer: "Nimbus 2000",
-    
-    }
-]
-
-
-let currentQuestion = 0;
-const question = questions[currentQuestion];
-
-document.querySelector("#question").textContent = questions[currentQuestion].question;
-document.querySelector("#btn1").textContent = questions[currentQuestion].answers[0];
-document.querySelector("#btn2").textContent = questions[currentQuestion].answers[1];
-document.querySelector("#btn3").textContent = questions[currentQuestion].answers[2];
-document.querySelector("#btn4").textContent = questions[currentQuestion].answers[3];
-
-
-// I tried to add the following if statements, and it messed up the question display and answer buttons
-
-let correct = 0;
-let incorrect = 0;
-
-document.querySelector("#answerChoices").addEventListener("click", function (){
-    console.log(this);
-
-
-}
-) // Use event delegation activity to figure out how to determine which button was clicked
 
 
 
 
-// $("#btn").on("click", function(){
-//     console.log(this);
-
-    // if (currentQuestion[0]) {
-    //     if (){ //or should i switch this to answer[3] === true?
-    //         correct++;
-    //     } else {
-    //         incorrect++;
-    //     }
-    // }
-    
-    // if (currentQuestion[1]) {
-    //     if (answer[1]){
-    //         correct++;
-    //     } else {
-    //         incorrect++;
-    //     }
-    // }
-    
-    // if (currentQuestion[2]) {
-    //     if (answer[1]){
-    //         correct++;
-    //     } else {
-    //         incorrect++;
-    //     }
-    // }
-    
-    // if (currentQuestion[3]) {
-    //     if (answer[2]){
-    //         correct++;
-    //     } else {
-    //         incorrect++;
-    //     }
-    // }
-    
-    // if (currentQuestion[4]) {
-    //     if (answer[1]){
-    //         correct++;
-    //     } else {
-    //         incorrect++;
-    //     }
-    // }
-// })
 
 
+
+
+
+
+
+ //   if (state === "still") {
+    //     // Change the data-state attribute's value
+    //     // There are two different ways this attribute can be set
+    //     element.dataset.state = "animate";
+    //     element.setAttribute("data-state", "animate");
+  
+    //     // Update the image's source to the string being stored in the data-animate attribute
+    //     element.setAttribute("src", element.dataset.animate);
+    //   } else {
+    //     // Change the attributes back to their non-animated values
+    //     element.dataset.state = "still";
+    //     element.setAttribute("src", element.dataset.still);
+    //   }
+
+
+
+// Use event delegation activity to figure out how to determine which button was clicked
 
 
 
